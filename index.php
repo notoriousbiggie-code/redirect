519f5e9c999b02c13b1383021299aee8a145869f
notoriousbiggie-code <62277883+notoriousbiggie-code@users.noreply.github.com>
2020-05-28 15:34:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
    <head>
-      <title>Clickfunnels.com</title>
-      <meta http-equiv = "refresh" content = "0; url = https://www.clickfunnels.com/" />
+      <title>One Funnel Away Challenge</title>
+      <meta http-equiv = "refresh" content = "0; url = https://onefunnelaway.com/challenge" />
    </head>
    <body>
       <p></p>
+<IFRAME src="https://onefunnelaway.com/challenge?cf_affiliate_id=2453624&affiliate_id=2453624" width="1" height="1" scrolling="auto" frameborder="0"></IFRAME>
    </body>
 </html>
