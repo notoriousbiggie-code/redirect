d5e31a84b87d986f8f3eeeef3df8aad5d7176490
notoriousbiggie-code <62277883+notoriousbiggie-code@users.noreply.github.com>
2020-05-28 15:17:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
-<?php 
-
-
-
-?> 
+<?php
+<html>
+   <head>
+      <title>Clickfunnels.com</title>
+      <meta http-equiv = "refresh" content = "0; url = https://www.clickfunnels.com/" />
+   </head>
+   <body>
+      <p></p>
+   </body>
+</html>
+?>
