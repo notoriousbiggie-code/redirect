84832139930934724b2464226013668f07f867be
notoriousbiggie-code <62277883+notoriousbiggie-code@users.noreply.github.com>
2020-05-28 15:01:27 +0530

    Update cf-upload.php

--- cf-upload.php
+++ cf-upload.php
@@ -1,10 +1,7 @@
-<!DOCTYPE html>
-<html>
-   <head>
-      <title>Clickfunnels.com</title>
-      <meta http-equiv = "refresh" content = "0; url = https://www.clickfunnels.com/" />
-   </head>
-   <body>
-      <p></p>
-   </body>
-</html>
+<?php 
+
+// Redirect browser 
+header("Location: http://www.geeksforgeeks.org"); 
+
+exit; 
+?> 
